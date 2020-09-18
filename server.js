c25189f727373de60db58a3aa1cb3365e5dbbe15
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-09-18 09:29:47 -0700

    new try for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -11,15 +12,16 @@ app.use("/users", require("./routes/userRouter"))
 app.use("/builds", require("./routes/build-routes"))
 app.use("/api", require("./routes/api-routes"))
 
-const PORT = process.env.PORT || 5000;
+
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, '../build')))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
