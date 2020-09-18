2fb2d1031e8218d68a80350fe5bfbbcf2dcd7bde
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-09-18 11:38:05 -0700

    removed favicon

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -14,7 +15,7 @@ app.use("/api", require("./routes/api-routes"))
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static('client/build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
