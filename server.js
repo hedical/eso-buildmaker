dfc4de80df17911ce9ad8bda7c83aacb4ca526b1
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-09-18 11:26:38 -0700

    moodified static and path in server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -15,7 +14,7 @@ app.use("/api", require("./routes/api-routes"))
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../build')))
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
